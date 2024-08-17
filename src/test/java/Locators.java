3a93c9f889338dfcb469cbfbc3c0b048be3012fa
Mohamed Elmaadawy <91470979+VOL1TZ1@users.noreply.github.com>
2024-08-17 10:33:33 +0300

    1st Commit

---
+++ src/test/java/Locators.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+
+public class Locators {
+    //Home Page
+    public static By SignInButtonHomePage =By.className("login");
+    public static By WomenButtonMainPage = By.className("sf-with-ul");
+    public static By TshirtButtonSubMenu = By.linkText("T-shirts");
+    //Login Page
+    public static By EmailTextField = By.id("email");
+    public static By PasswordTextField = By.id("passwd");
+    public static By SignInButtonSignInPage = By.id("SubmitLogin");
+    public static By WelcomeMessage = By.className("info-account");
+    public static By HomeButtonSignInPage = By.className("button-small");
+    //T-Shirt Page
+
+
+}
